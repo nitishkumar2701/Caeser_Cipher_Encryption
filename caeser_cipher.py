28c23d3a2e12d42c7ffe14d45142718375539651
Nitish Kumar S <62009174+nitishkumar2701@users.noreply.github.com>
2024-08-26 13:50:39 +0530

    Add files via upload

---
+++ caeser_cipher.py
@@ -0,0 +1,36 @@
+import string
+textLetterList = []
+encodedList = []
+decodedList = []
+alphabet_list = list(string.ascii_lowercase)
+
+def encrypt(text,shift):
+    for letter in text:
+        if letter in alphabet_list: 
+            shift_index = (alphabet_list.index(letter) + shift) % 26
+            encodedList.append(alphabet_list[shift_index])
+        else:
+            encodedList.append(letter)
+    print("Encode message - " + ''.join(encodedList))
+
+def decrypt(text,shift):
+    for letter in text:
+        if letter in alphabet_list: 
+            shift_index = (alphabet_list.index(letter) - shift) % 26
+            decodedList.append(alphabet_list[shift_index])
+        else:
+            decodedList.append(letter) 
+    print("Decode message - " + ''.join(decodedList))
+
+print("Caeser Cipher Encryption in Python")
+
+input_text = input("Type the message \n").lower()
+input_shift = int(input("Type the shift number \n"))
+direction = input("Do you want to encrypt or decrypt this message ? \n Type encode to encrypt/ decode to decrypt \n").lower()
+
+if(direction == "encode"):
+    encrypt(input_text,input_shift)
+elif(direction == "decode"):
+    decrypt(input_text,input_shift)
+else:
+    print("Please enter a valid option")
